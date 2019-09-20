3eb877668c79862b82550fa386ad794c5a7208b6
Ricardo Antonio Caro Cardona <ricardo.caro9@hotmail.com>
2019-09-20 10:57:05 -0500

    correccion e6 otra vez

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app = express()
 
 app.use("/", serveStatic(join(__dirname + '/dist'))) 
 
-const puerto = process.env.PORT || 8080; 
+const puerto = process.env.PORT || 5000; 
 app.listen(puerto) 
 
 console.log ('servidor iniciado' + puerto);
