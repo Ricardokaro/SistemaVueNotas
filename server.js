530a4063d6891710c377f9f6ccab579be429d106
Ricardo Antonio Caro Cardona <ricardo.caro9@hotmail.com>
2019-09-20 11:45:01 -0500

    correccion e6 otra vez 3

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-import express from 'express'
-import serveStatic from 'serve-static' 
-import { join } from "path"
+const express = require('express')
+const serveStatic = require('serve-static') 
+const path = require("path")
 
 app = express()
 
-app.use("/", serveStatic(join(__dirname + '/dist'))) 
+app.use("/", serveStatic(path.join(__dirname + '/dist'))) 
 
 const puerto = process.env.PORT || 5000; 
 app.listen(puerto) 
