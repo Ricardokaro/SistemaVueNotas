fda54f844bcb08b18249fca26f07a499fb630383
Ricardo Antonio Caro Cardona <ricardo.caro9@hotmail.com>
2019-09-20 10:14:27 -0500

    correccion en el  archivo serverjs

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const serveStatic = require ('serve-static'); 
 const path = require("path");
 
-aplicación = express (); 
+app = express (); 
 
-app.use ("/", serveStatic(path.join(__dirname + '/sistema/dist'))); 
+app.use ("/", serveStatic(path.join(__dirname + '/dist'))); 
 
 const puerto = proceso.env.PORT || 8080; 
 app.listen(puerto); 
