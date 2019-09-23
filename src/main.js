bf21fb3e4f084c67e40ff0d46a27821103a7d285
Ricardo Antonio Caro Cardona <ricardo.caro9@hotmail.com>
2019-09-23 10:51:48 -0500

    cambie la ruta para consumir la api rest otra vez

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import store from './store'
 import axios from 'axios'
 
 Vue.config.productionTip = false
-axios.defaults.baseURL = 'http://http://ricardocaro-001-site1.ctempurl.com/';
+axios.defaults.baseURL = 'ricardocaro-001-site1.ctempurl.com/';
 new Vue({
   router,
   store,  
